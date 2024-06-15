d1ae19901116cac235a957858cbe3a4c8a7d7fe2
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-06-15 18:17:49 -0500

    feat: :sparkles: Descripcion de detail
    sa realizo la descripcion del producto de detail

--- js/components/section.js
+++ js/components/section.js
@@ -17,3 +17,23 @@ export const titleProductDetail = async({ data:dataUpdate } = res)=>{
         </article>
         `;
 }
+
+export const descriptionProductDetail = async ({data:dataUpdate} = res) => {
+    if (dataUpdate.product_description != null){
+        let createDescripHTML = async() => {
+            let description = await dataUpdate.product_description;
+            let trunDescription = description;
+            if (description.length > 150) {
+                trunDescription = description.substring(0, 150) + '... <strong id = "leerMasOption"> Leer más.</strong>';
+            }
+            return `${trunDescription}`;
+        }
+
+        return /*html*/`
+        <article class="product__information">
+            <p id = "informationProduct">${await createDescripHTML()}</p>
+            <hr>
+        </article>
+        `;
+    }else return null;
+    };
--- js/detail.js
+++ js/detail.js
@@ -1,9 +1,11 @@
 import { galleryCategory } from "./components/gallery.js";
-import { titleProductDetail } from "./components/section.js";
+import { titleProductDetail, descriptionProductDetail} from "./components/section.js";
 import { getProductId } from "./module/detail.js";
 
 let main__section_gallery = document.querySelector("#main__section__gallery");
 let main__section__title = document.querySelector("#main__section__title");
+let mainsectiondescription = document.querySelector("#main__section__description");
+
 
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
@@ -12,6 +14,7 @@ addEventListener("DOMContentLoaded", async(e)=>{
     let info = JSON.parse(localStorage.getItem(id));
     main__section_gallery.innerHTML = await galleryCategory(info);
     main__section__title.innerHTML = await titleProductDetail(info);
+    mainsectiondescription.innerHTML = await descriptionProductDetail(info);
 
 
     //main__section_gallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)))
@@ -28,4 +31,12 @@ addEventListener("DOMContentLoaded", async(e)=>{
     //     ...dataUpdate
     // } = data;
     // console.log(dataUpdate);
+    let optionViewMore = document.querySelector("#leerMasOption");
+    let viewMore = document.querySelector("#informationProduct");
+
+    optionViewMore.addEventListener('click', async (e) => {
+        let complet = info.data.product_description;
+        viewMore.textContent = complet;
+    })
+
 })
