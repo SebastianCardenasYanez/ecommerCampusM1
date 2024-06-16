d1b50e589de769fdfaf4bf08274d16eceb662902
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-06-15 20:38:28 -0500

    feat: :construction: precio del footer
    se creo un evento que cuando se modifica la cantidad de productos, aumenta el precio o disminuye, faltan algunos retoques

---
+++ js/components/footer.js
@@ -0,0 +1,30 @@
+
+
+export const priceDetails = async({data: dataUpdate} = res) => {
+    
+
+    if (dataUpdate.product_price ==  null) {
+        return /*html*/`
+    <li>    
+        <a href="checkout.html">
+            <img src="../storage/img/shopping-cart.svg">
+            <span>Add to Cart No Price</span>
+        </a>
+    </li>`
+}
+    if (dataUpdate.product_original_price ==  null){
+        return /*html*/`
+    <li>    
+        <a href="checkout.html">
+            <img src="../storage/img/shopping-cart.svg">
+            <span id= "precio__total">Add to Cart ${dataUpdate.product_price}</span>
+        </a>
+    </li>`
+    }
+    return /*html*/`
+    <li>    
+    <a href="checkout.html">
+        <img src="../storage/img/shopping-cart.svg">
+        <span id= "precio__total" >Add to Cart ${dataUpdate.product_price}<del><sub>${dataUpdate.product_original_price}</sub></del></span>
+    </a>
+</li>`};
--- js/detail.js
+++ js/detail.js
@@ -1,11 +1,12 @@
 import { galleryCategory } from "./components/gallery.js";
 import { titleProductDetail, descriptionProductDetail} from "./components/section.js";
 import { getProductId } from "./module/detail.js";
+import {priceDetails} from "./components/footer.js";
 
 let main__section_gallery = document.querySelector("#main__section__gallery");
 let main__section__title = document.querySelector("#main__section__title");
 let mainsectiondescription = document.querySelector("#main__section__description");
-
+let footer__ul = document.querySelector(".footer__ul");
 
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
@@ -15,7 +16,8 @@ addEventListener("DOMContentLoaded", async(e)=>{
     main__section_gallery.innerHTML = await galleryCategory(info);
     main__section__title.innerHTML = await titleProductDetail(info);
     mainsectiondescription.innerHTML = await descriptionProductDetail(info);
-
+    console.log(priceDetails(info))
+    footer__ul.innerHTML = await priceDetails(info);
 
     //main__section_gallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)))
     // let {data} = res;
@@ -34,6 +36,7 @@ addEventListener("DOMContentLoaded", async(e)=>{
     let optionViewMore = document.querySelector("#leerMasOption");
     let viewMore = document.querySelector("#informationProduct");
 
+
     optionViewMore.addEventListener('click', async (e) => {
         let complet = info.data.product_description;
         viewMore.textContent = complet;
@@ -44,17 +47,25 @@ addEventListener("DOMContentLoaded", async(e)=>{
     let decreaseButton = document.querySelector("#decreaseQuantity");
         let increaseButton = document.querySelector("#increaseQuantity");
         let quantitySpan = document.querySelector("#quantity");
+        let precio__total = document.querySelector("#precio__total")
 
         decreaseButton.addEventListener('click', async e => {
         let quantity = parseInt(quantitySpan.textContent);
+        let precioentero = parseFloat(info.data.product_price.replace('$',''))
         if(quantity > 1){
             quantitySpan.textContent = quantity - 1;
+            quantity = parseInt(quantitySpan.textContent);
+            precio__total.innerHTML = `Add to Cart $${quantity * precioentero}`
         };
         });
 
         increaseButton.addEventListener('click', async e => {
         let quantity = parseInt(quantitySpan.textContent);
+        let precioentero = parseFloat(info.data.product_price.replace('$',''))
         quantitySpan.textContent = quantity + 1;
+        quantity = parseInt(quantitySpan.textContent);
+        precio__total.innerHTML = `Add to Cart $${quantity * precioentero}`
+        
         });
 
 })
