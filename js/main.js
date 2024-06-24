42a4c7806b3bd60f1d587e39e5c05086a5f35db8
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-06-24 16:41:39 -0500

    feat: :sparkles: funcion numero de items en carrito
    en el footer del index se muestra la cantidad de items en el carrito y lo redirecciona a este

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ import { getAllProductName, getAllCategory, getAllClothesUnder10} from "./module
 let input__search = document.querySelector("#input__search");
 let main__article = document.querySelector(".main__article");
 let nav__ul = document.querySelector(".nav__ul");
+let totalItems = document.querySelector('#totalItems');
 
 addEventListener("DOMContentLoaded", async e=>{
     if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
@@ -27,3 +28,16 @@ input__search.addEventListener("change", async e => {
     let res = await getAllProductName(data)
     main__article.innerHTML = galleryIndex(res, params.get('id'));
 });
+
+let keys = Object.keys(localStorage);
+let items = 0
+keys.forEach(key => {
+    let local = JSON.parse(localStorage.getItem(key))
+    console.log(local);
+    if(local.checkout){
+        items++
+    }
+    if(items !== 0){
+        totalItems.textContent = items
+    }
+})
