a70d42cfaf7cfaff03293a6cdf9ae5a1f2857b7a
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-07-04 06:58:27 -0500

    chore: :truck: key update

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': '258e34c90fmsh5911dd7c4d48b83p129294jsn87b55572980d',
+    'x-rapidapi-key': '50971097b5msh3d4af24a0f42f35p1b73b0jsn894ade3816fa',
     'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com',
 }
